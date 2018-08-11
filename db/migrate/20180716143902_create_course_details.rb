67519eeb34bb91e220873d45b3613b0dd6896a5a
Eno Bassey <ennyboy@gmail.com>
2018-08-11 18:29:42 +0100

    rename course details

--- db/migrate/20180716143902_create_course_details.rb
+++ db/migrate/20180716143902_create_course_details.rb
@@ -1,5 +1,5 @@
 # Migration to create course details table
-class CreateCourseDetail < ActiveRecord::Migration[5.2]
+class CreateCourseDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :course_details do |t|
       t.integer :day_number, :default => 0
